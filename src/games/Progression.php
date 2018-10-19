ea1d190d4b6bbaa6e4d010481f99d4f95f284238
Stanislav2014 <35069509+Stanislav2014@users.noreply.github.com>
2018-10-19 13:45:44 +0300

    Update and rename Progressive.php to Progression.php
    fix name to Progression.php

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -1,38 +1,38 @@
 <?php
 
-namespace Braingames\Progressive;
+namespace Braingames\Progression;
 
 use function \Braingames\Cli\run;
 
 const GAMELINE = 'What number is missing in this progression?';
 const LENGTH = 10;
 
-function createProgressive($first, $step, $length)
+function createProgression($first, $step, $length)
 {
     for ($i = 1; $i < LENGTH; $i++) {
-        $arrProgressive[] = ($first + $step) * $i;
+        $arrProgression[] = ($first + $step) * $i;
     }
 
-    return($arrProgressive);
+    return($arrProgression);
 }
 
-function runFindInProgressive()
+function runFindInProgression()
 {
     $create = function () {
 
         $first = rand(1, 10);
         $step = rand(1, 3);
         
-        $arrProgressive = createProgressive($first, $step, LENGTH);
+        $arrProgression = createProgression($first, $step, LENGTH);
 
         $key = rand(0, LENGTH - 1);
 
-        $arrWithQuestion = $arrProgressive;
+        $arrWithQuestion = $arrProgression;
         $arrWithQuestion[$key] = "..";
 
         $question = implode(' ', $arrWithQuestion);
 
-        $rightAnswer = $arrProgressive[$key];
+        $rightAnswer = $arrProgression[$key];
 
         return [$question, (string)$rightAnswer];
     };
