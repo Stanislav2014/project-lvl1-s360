03c2905897c8456c5beba4ec2d9b8e17038f18bb
Stanislav2014 <35069509+Stanislav2014@users.noreply.github.com>
2018-10-19 13:58:36 +0300

    Update Prime.php

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -6,11 +6,11 @@ use function \Braingames\Cli\run;
 const GAMELINE = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 function isPrime($num)
 {
-  for($i = 2; $i <= sqrt($num); $i++) {
-      if($num % $i == 0) {
-          return false;
-      }
-  }
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
     return true;
 }
 
